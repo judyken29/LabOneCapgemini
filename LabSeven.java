4ade79affa3dfbcebca3931624c4862fd775e528
judyken29 <81622421+judyken29@users.noreply.github.com>
2021-04-04 18:50:20 +0530

    Create LabSeven.java

---
+++ LabSeven.java
@@ -0,0 +1,34 @@
+ package lab;
+
+import java.util.Scanner;
+
+
+public class LabSeven {
+    public static void main (String[] args) {
+       
+        int number;
+        boolean increasingNumber=false;
+        Scanner input = new Scanner(System.in);
+        number = input.nextInt();
+        increasingNumber = checkNumber(number);
+        System.out.println(increasingNumber);
+    }
+    public static boolean checkNumber(int number) {
+        boolean increasing = false;
+        while(number>0) {
+            int lastDigit = number % 10;
+            number /= 10;
+            int nextLastDigit = number % 10;
+            
+            if(nextLastDigit<=lastDigit) {
+                increasing=true;
+            }
+            else {
+                increasing=false;
+                break;
+            }
+            
+        }
+        return increasing;
+    }
+}

5764bdb51a707ff345945b71899795e68a2515a4
judyken29 <81622421+judyken29@users.noreply.github.com>
2021-04-04 18:49:38 +0530

    Create LabFive.java

---
+++ LabFive.java
@@ -0,0 +1,26 @@
+package lab;
+
+class LabFive{
+    
+   
+    static void result(int N)
+    {     
+        
+        for (int num = 0; num < N; num++)
+        {     
+            
+            if (num % 3 == 0 && num % 5 == 0)
+                System.out.print(num + " ");
+        }
+    }
+       
+    
+    public static void main(String []args)
+    {
+    
+        int N = 100;
+           
+        
+        result(N);
+    }
+}
